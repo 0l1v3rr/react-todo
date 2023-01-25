dce3d3d90d0f5cbf80bd9efb0355b88a0fbe7d6e
Olivér <oliver.mrakovics@gmail.com>
2023-01-25 15:14:19 +0100

    add useTaskData hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,133 +1,14 @@
 import Flex from "./components/layout/Flex";
 import Labels from "./pages/Labels";
 import Meter from "./pages/Meter";
-import { Label, Task, TaskStatus } from "./types/task";
-import { useState, useCallback } from "react";
-import { defaultLabels } from "./const/labels";
+import { useState } from "react";
 import CardContainer from "./components/cards/CardContainer";
-import { createUUID } from "./utils/uuid";
 import { TransferData } from "./types/dataTransfer";
+import { useTasksData } from "./hooks/useTasksData";
 
 const App = () => {
   const [draggedData, setDraggedData] = useState<TransferData>();
-  const [labels] = useState<Label[]>(defaultLabels);
-  const [tasks, setTasks] = useState<Task[]>([
-    {
-      id: "1",
-      position: 1,
-      labels: [labels[0]],
-      status: "To Do",
-      title: "Test task to do",
-    },
-    {
-      id: "4",
-      position: 2,
-      labels: [labels[4], labels[3]],
-      status: "To Do",
-      title: "Test task with longer title and emoji and rice 🥰",
-    },
-    {
-      id: "2",
-      position: 1,
-      labels: [],
-      status: "In Progress",
-      title: "Test task 2",
-    },
-    {
-      id: "3",
-      position: 1,
-      labels: [labels[2]],
-      status: "Done",
-      title: "Test task 2",
-    },
-  ]);
-
-  const filterTasks = useCallback(
-    (status: TaskStatus) =>
-      [...tasks]
-        .filter((t) => t.status === status)
-        .sort((a, b) => a.position - b.position),
-    [tasks]
-  );
-
-  const getNextPosition = useCallback(
-    (status: TaskStatus) => {
-      return filterTasks(status).length + 1;
-    },
-    [filterTasks]
-  );
-
-  const editTask = useCallback(
-    (id: string, task: Task) => {
-      const index = tasks.findIndex((task) => task.id === id);
-      const newArr = [...tasks];
-      newArr[index] = task;
-      setTasks(newArr);
-    },
-    [tasks]
-  );
-
-  const shiftTasks = useCallback(
-    (
-      toStatus: TaskStatus,
-      taskId: Task["id"],
-      index: number,
-      originalIndex: number
-    ) => {
-      // the index of the task in the FULL tasks array
-      const taskIndex = tasks.findIndex((task) => task.id === taskId);
-
-      const filteredTasks = filterTasks(toStatus);
-      const fromStatusTasks = filterTasks(tasks[taskIndex].status);
-
-      const updatedTask: Task = {
-        ...tasks[taskIndex],
-        status: toStatus,
-        position: index + 2,
-      };
-
-      const newTasks = [...tasks];
-
-      // increment the position by 1 "under" the new element
-      for (let i = index + 1; i < filteredTasks.length; i++) {
-        const idx = newTasks.findIndex(
-          (task) => task.id === filteredTasks[i].id
-        );
-
-        const { position } = newTasks[idx];
-        newTasks[idx] = { ...newTasks[idx], position: position + 1 };
-      }
-
-      // decrement the position by 1 in the column where the task originates from
-      for (let i = originalIndex + 1; i < fromStatusTasks.length; i++) {
-        const idx = newTasks.findIndex(
-          (task) => task.id === fromStatusTasks[i].id
-        );
-
-        const { position } = newTasks[idx];
-        newTasks[idx] = { ...newTasks[idx], position: position - 1 };
-      }
-
-      // remove and push the updated task
-      newTasks.splice(taskIndex, 1);
-      newTasks.push(updatedTask);
-
-      setTasks(newTasks);
-    },
-    [tasks, filterTasks, setTasks]
-  );
-
-  const newTask = useCallback(
-    (task: Task) => {
-      setTasks((prev) => {
-        task.position = getNextPosition(task.status);
-        task.id = createUUID();
-
-        return [...prev, task];
-      });
-    },
-    [getNextPosition]
-  );
+  const { newTask, shiftTasks, editTask, filterTasks, labels } = useTasksData();
 
   return (
     <main
